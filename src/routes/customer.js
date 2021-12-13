8daf4a15585d0544cfa8bd780c499ead8614e37d
Cristian <rojascristian822@gmail.com>
2021-12-12 23:50:07 -0500

    visualizacion

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -4,5 +4,5 @@ const router =express.Router();
 const customerC=require('../controllers/customercontroller');
 router.get('/',customerC.list);
 router.post('/add',customerC.save);
-
+router.get('/delete',customerC.delete);
 module.exports=router;
