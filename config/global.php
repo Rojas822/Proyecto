92236c1d862ca8574e8df8d4408203e2e531e990
Rojas822 <rojascristian822@gmail.com>
2022-03-21 16:38:45 -0500

    main

--- config/global.php
+++ config/global.php
@@ -1,21 +1,21 @@
 <?php 
 //ip de la pc servidor base de datos
-define("DB_HOST", "localhost");
+define("DB_HOST", "b1czbpsjr5iuhtqwrbqm-mysql.services.clever-cloud.com");
 
 // nombre de la base de datos
-define("DB_NAME", "sis_school");
+define("DB_NAME", "b1czbpsjr5iuhtqwrbqm");
 
 
 //nombre de usuario de base de datos
-define("DB_USERNAME", "root");
+define("DB_USERNAME", "unu7hkd6dzkieaz6");
 
 //conraseña del usuario de base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "unu7hkd6dzkieaz6");
 
 //codificacion de caracteres
 define("DB_ENCODE", "utf8");
 
 //nombre del proyecto
-define("PRO_NOMBRE", "CompartiendoCodigos");
+//define("PRO_NOMBRE", "CompartiendoCodigos");
  
  ?> 
