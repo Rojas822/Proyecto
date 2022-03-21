075e92046502e469921c2664570c5c6174d16eba
DuvanJC <duvan.camargo02@uptc.edu.co>
2022-03-21 00:29:38 -0500

    restart

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(morgan('dev'));
 app.use(myConnection(mysql, {
   host: 'localhost',
   user: 'root',
-  password: 'fines',
+  password: '',
   port: 3306,
   database: 'colegio'
 }, 'single'));
